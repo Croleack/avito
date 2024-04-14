f716a06db57ddbfa3c993fb54362acc5cb7943dc
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-04-14 23:14:43 +0500

    start

---
+++ avitoTestProject/avitoTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  avitoTestProjectApp.swift
+//  avitoTestProject
+//
+//  Created by Enzhe Gaysina on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct avitoTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
